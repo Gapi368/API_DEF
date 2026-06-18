8d34548c83197ced419025fce98e8f56cd1a0fc7
Gapi368 <alondraivethpinalesgalvan@gmail.com>
2026-06-17 18:22:36 -0600

    Add prioridad field to f.tarea.entity

--- src/f.tareas/entities/f.tarea.entity.ts
+++ src/f.tareas/entities/f.tarea.entity.ts
@@ -33,4 +33,7 @@ export class FTarea {
     @ManyToOne(() => Cmateria, (materia) => materia.tareas)
     @JoinColumn({ name: 'tarea_materia' })
     materia!: Cmateria;
+
+    @Column() 
+    prioridad!: number;
 }
